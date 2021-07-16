e1789754d6ee3e79b858d80a739d4e4a36cb05fa
otavio <oavelar@student.42lisboa.com>
2021-07-16 14:51:45 +0100

    philo run just with 2 , more than 2 make confusion

--- srcs/action.c
+++ srcs/action.c
@@ -6,7 +6,7 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:59:06 by oavelar           #+#    #+#             */
-/*   Updated: 2021/07/13 13:18:28 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/07/16 14:49:30 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,6 @@ void    philo_fork(t_philo *p)
     take_fork(p, p->left_fork, p->right_fork , p->number);
     if (p->data && get_time())
     printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number + 1);
-    /*if (p->right_fork && get_time())
-        printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number);
-    
-    pthread_mutex_lock(p->left_fork);
-    pthread_mutex_lock(p->right_fork);
-    pthread_mutex_lock(p->number);
-    printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number);
-    pthread_mutex_unlock(p->old_state);*/
 }
 
 void    philo_eat(t_philo *p)
@@ -34,17 +26,9 @@ void    philo_eat(t_philo *p)
     if (p->data->fork[p->left_fork] == p->number
         && p->data->fork[p->right_fork] == p->number)
     {
-       /* p->last_eat = get_time();
-        printf(YEL"[%lld] Philosopher's %d is eating\n", get_time() - p->data->base_time, p->number);
-    }*/
-    //pthread_mutex_lock(p->state);
     p->last_eat = get_time();
     printf(YEL"[%lld] Philosopher's %d is eating\n", get_time() - p->data->base_time, p->number + 1);
-    //print_state(p, get_time() - p->data->base_time, p->number);
     p->eat_cnt++;
-    //pthread_mutex_unlock(p->data->mutex_id);
-    //pthread_mutex_unlock(p->left_fork);
-   //pthread_mutex_unlock(p->right_fork);
     }
     sleep_time(p->data->time_to_eat);
 }
@@ -54,25 +38,14 @@ void		philo_sleep(t_philo *p)
     p->last_eat = get_time();
     printf(GRE"[%lld] Philosopher's %d is sleeping\n", get_time()
         - p->data->base_time, p->number + 1);
-	//printf("is "YEL"sleeping"GRE"!\n");
-    //print_state(p, get_time() - p->data->base_time, p->number);
     other_fork(p, p->right_fork, p->left_fork);
     sleep_time(p->data->time_to_sleep);
-	/*pthread_mutex_lock(p->mutex_monitor);
-	printf("%lld_in_ms %d is sleeping\n", get_time() - p->time, p->id);
-	pthread_mutex_unlock(p->mutex_monitor);
-	sleep_time(p->time_to_sleep);*/
 }
 
 void    philo_think(t_philo *p)
 {
     printf(BLU"[%lld] Philosopher's %d is thinking\n", get_time()
        - p->data->base_time, p->number + 1);
-     //print_state(p->data, get_time() - p->data->base_time, p->number);
-	//printf("is "YEL"thinking"GRE"!\n");
-    /*pthread_mutex_lock(p->mutex_monitor);
-    printf("%lld_in_ms %d is thinking\n", get_time() - p->time, p->id);
-    pthread_mutex_unlock(p->mutex_monitor);*/
 }
 
 void    *routine_philo(void *p)
--- srcs/philo_fork.c
+++ srcs/philo_fork.c
@@ -6,34 +6,11 @@
 /*   By: oavelar <oavelar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:49:41 by oavelar           #+#    #+#             */
-/*   Updated: 2021/07/13 13:23:44 by oavelar          ###   ########.fr       */
+/*   Updated: 2021/07/16 14:50:15 by oavelar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-/*void	print_state(t_philo *p, int time, int num)
-{
-    (void)num;
-	if (p->data->monitor_flag && (time = 1))
-       	printf(RED"[%lld] Philosopher's %d died\n", get_time()
-        		- p->data->base_time, p->number + 1);
-		//print_time_num(p, time, number + 1, "died\n");
-	else if (!p->data->monitor_flag && (time = 1))// && state == TAKE)
-        printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number + 1);
-		//print_time_num(global, time, number + 1, "has taken a fork\n");
-	else if (!p->data->monitor_flag && (time = 1))// && state == EAT)
-       printf(YEL"[%lld] Philosopher's %d is eating\n", get_time() - p->data->base_time, p->number + 1);
-        //print_time_num(p, time, number + 1, "is eating\n");
-	else if (!p->data->monitor_flag && (time = 1)) //&& state == SLEEP)
-		printf(GRE"[%lld] Philosopher's %d is sleeping\n", get_time()
-        - p->data->base_time, p->number + 1);
-        //print_time_num(p, time, number + 1, "is sleeping\n");
-	else if (!p->data->monitor_flag && (time = 1)) //&& state == THINK)
-		printf(BLU"[%lld] Philosopher's %d is thinking\n", get_time()
-        - p->data->base_time, p->number + 1);
-        //print_time_num(p, time, number + 1, "is thinking\n");
-}*/
 
 void    take_fork(t_philo *p, int left, int right, int p_num)
 {
@@ -44,8 +21,8 @@ void    take_fork(t_philo *p, int left, int right, int p_num)
     p->data->fork[left] = p_num;
     p->data->fork[right] = p_num;
     //print_state(p, get_time() - p->data->base_time, p->number);
-    //if (p->data && !p->number)
-    printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number + 1);
+   // if (p->data && p->number % 2)
+    //printf(MAG"[%lld] Philosopher's %d has taken a fork\n", get_time() - p->data->base_time, p->number + 1);
 }
 
 void	other_fork(t_philo *p, int right_f, int left_f)
@@ -55,86 +32,3 @@ void	other_fork(t_philo *p, int right_f, int left_f)
 	pthread_mutex_unlock(&p->data->mutex_id[right_f]);
 	pthread_mutex_unlock(&p->data->mutex_id[left_f]);
 }
-// SEGMENTTAKOPGJOPSGPOSGRK MERDA
-
-/*void    *ft_philo(void *phi)
-{
-    t_philo     *p;
-    pthread_t    main;
-    int          count;
-
-    count = 0;
-    p = (t_philo *)phi;
-    if (p->id % 2 != 0)
-        sleep_time(p->time_to_eat);
-    pthread_create(&main, NULL, &monitor, (void *)p);
-    pthread_detach(main);
-    while (p->live && (!p->end_eat || count < p->end_eat_amount))
-    {
-        philo_fork(p);
-        philo_eat(p);
-        philo_sleep(p);
-        philo_think(p);
-        count++;
-    }
-    p->live = false;
-    if (p->end_eat && count == p->end_eat_amount)
-        p->data->meals++;
-    return (NULL);
-}
-
-void    delete_philo(int end, pthread_t **ph2)
-{
-    int count;
-
-    count = 0;
-    while (count < end)
-        pthread_detach((*ph2)[count++]);
-}
-
-void    ft_check_meals(t_philo *in, pthread_t **ph2)
-{
-    int idx;
-    int end;
-
-    idx = 0;
-    end = in[idx].number_of_philo;
-    while (!in->data->stop && (in->data->meals) < end)
-        sleep_time(1);
-    if (in->data->stop || in->data->meals == end)
-        delete_philo(end, ph2);
-    if (!in->data->stop && in->data->meals == end)
-    {
-        pthread_mutex_lock(in->mutex_monitor);
-        printf("philo all ate!\n");
-    }
-}
-
-void    ft_check_die(t_philo *in, pthread_t **ph2)
-{
-    int end;
-
-    end = in[0].number_of_philo;
-    while (!in->data->stop)
-        sleep_time(1);
-    if (in->data->stop)
-        delete_philo(end, ph2);
-}
-
-int init_process(t_philo *in, pthread_t **ph2)
-{
-    int index;
-
-    index = 0;
-    while (index < in->number_of_philo)
-    {
-        pthread_create(&((*ph2)[index]), NULL, ft_philo, (void *)&(in[index]));
-        index++;
-    }
-    if (in->end_eat)
-        ft_check_meals(in, ph2);
-    else
-        ft_check_die(in, ph2);
-    return (1);
-}
-*/
